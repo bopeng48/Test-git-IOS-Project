080857eb398d8d38a8a7b322d2626322db9bffc7
Bo Peng <bopeng@Bo-Pengs-MacBook-Pro-3.local>
2014-08-17 22:55:48 -0700

    added a test class

---
+++ BPTest.h
@@ -0,0 +1,13 @@
+//
+//  BPTest.h
+//  Git Test
+//
+//  Created by Bo Peng on 8/17/14.
+//  Copyright (c) 2014 Bo Peng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BPTest : NSObject
+
+@end
---
+++ BPTest.m
@@ -0,0 +1,13 @@
+//
+//  BPTest.m
+//  Git Test
+//
+//  Created by Bo Peng on 8/17/14.
+//  Copyright (c) 2014 Bo Peng. All rights reserved.
+//
+
+#import "BPTest.h"
+
+@implementation BPTest
+
+@end
